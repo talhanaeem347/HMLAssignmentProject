51c133b21455c3ef983c6c12e9f2b7bdf6e455a8
FaheemRafiq <faheemkhan198742@gmail.com>
2023-05-09 16:02:02 +0500

    Sticky Header

---
+++ main.js
@@ -0,0 +1,11 @@
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY || window.pageYOffset;
+    if (Math.floor(scrollPosition) > 100) {
+        header.style.position = 'sticky';
+        header.style.top = '0';
+    }else{
+        header.style.position = 'relative';
+    }
+});
